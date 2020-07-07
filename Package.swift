058d4d66860193122f274cce70e5780e793e373c
Frizlab <fload@me.com>
2020-07-07 15:07:48 +0200

    Bump min required Swift version and set all supported OS versions
    This is a fix for compatibility with BMO and Xcode 12

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,6 +7,12 @@ import PackageDescription
 
 let package = Package(
 	name: "AsyncOperationResult",
+	platforms: [
+		.macOS(.v10_10),
+		.iOS(.v8),
+		.tvOS(.v9),
+		.watchOS(.v2)
+	],
 	products: [
 		.library(
 			name: "AsyncOperationResult",
